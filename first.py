fb9b5f9e43df23f55249ba42bb2a65c0985fc1a9
anilneti <anilneti@gmail.com>
2018-10-26 07:53:21 +0000

    python script

---
+++ first.py
@@ -0,0 +1,41 @@
+#!/bin/python2.7
+print('Hello,World')
+x="anil"
+print ('print name ' +x)
+### + Operator
+x = "This is "
+y = "my first program"
+z = x + y
+print (z)
+#### numbers
+x = 1
+y = 1.234
+z = 1j
+print(type(x))
+print(type(y))
+print(type(z))
+x = int(1)
+print x
+### strings
+x = "hello, good morning"
+print x[1:4]
+print(len(x))
+print(x.lower())
+print(x.upper())
+print(x.split(","))
+print(x.split(":"))
+print(x.replace("morning","day"))
+### accepting input from user
+print("Enter the first number")
+x = input()
+print("==========================")
+print ("Enter the second number")
+y = input()
+print("==========================")
+print(x+y)
+#### Operators
+print ("Enter first number")
+x = input()
+print ( x > 2)
+print ("Enter second number")
+y = input()
